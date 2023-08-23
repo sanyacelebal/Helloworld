5d67f10dd9990423bd95efbff48be28d4788e48a
sanya gupta <sanya.gupta@celebaltech.com>
2023-08-23 11:29:17 +0530

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,79 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*   int a = 10;
+               float b = 12.78;
+               double h = 23.54; 
+               char c = 'A';
+               string d = "sanya gupta";
+               long e = 100000000000;
+               bool f = true;
+               decimal g = (double)45.6;
+               Console.WriteLine(a);
+               Console.WriteLine(b);
+               Console.WriteLine(c);
+               Console.WriteLine(d);
+               Console.WriteLine(e);
+               Console.WriteLine(f);
+               Console.WriteLine(g);
+               Console.WriteLine(h);  */
+
+
+            // Arithmetic operators//
+           /*  int a = 5;
+             int b = 3;
+             Console.WriteLine("addition :"+(a+b));
+             Console.WriteLine("subtraction :"+(a-b));
+             Console.WriteLine("multiplication:"+(a*b));
+             Console.WriteLine("division:"+(a/b));
+             Console.WriteLine("Modulus:"+(a%b));
+             Console.WriteLine("Increment:"+(a++));
+             Console.WriteLine(a);
+             Console.WriteLine("Decrement:"+(a--));
+             Console.WriteLine(a);   */
+
+
+            //Assignmnet operator//
+            /*   int a=5;
+                int b = a;
+                Console.WriteLine("value of b :"+b);
+                Console.WriteLine(a+=3);
+                Console.WriteLine(a-=3);
+                Console.WriteLine(a *= 3);
+                Console.WriteLine(a /= 3);
+                Console.WriteLine(a &= 3);
+                Console.WriteLine(a |= 3);
+                Console.WriteLine(a<<=3);
+                Console.WriteLine(a>>=3); */
+
+
+            // Comparison operators//
+            /*   int a = 5;
+               int b = 10;
+               Console.WriteLine(a == b);
+               Console.WriteLine(a != b);
+               Console.WriteLine(a > b);
+               Console.WriteLine(a < b);
+               Console.WriteLine(a >= b);
+               Console.WriteLine(a <= b);*/
+
+
+            // Logical operators//
+            /*  int a = 5;
+              Console.WriteLine(a >10 && a>2);
+              Console.WriteLine(a>10 || a>2); 
+              Console.WriteLine(!(a>10 && a>2));  */
+
+            /*  int a = 1;
+
+              Console.WriteLine((a++) + (++a)); */
+
+          
+        }
+    }
+}
